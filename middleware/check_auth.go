2a2daed04cc009e4974e8fea91137fcee59d9cdd
user <kurganov.v@dornet.ru>
2024-09-08 22:10:12 +0300

    CG-22 WIP

--- middleware/check_auth.go
+++ middleware/check_auth.go
@@ -1,12 +1,18 @@
 package middleware
 
 import (
+	"card-game/session"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 )
 
 func CheckAuth(c *fiber.Ctx) error {
 	token := c.Get("Authorization")
+	session, _ := session.Session.Get(c)
+
+	token = session.Get("Dsada")
+
+	fmt.Println(token)
 	fmt.Println(token)
 
 	return c.Next()
